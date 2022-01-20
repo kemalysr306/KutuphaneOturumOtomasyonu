8464a9ce07a6af86caf7225ed9c0f37a9f3261a4
HP <HP@DESKTOP-8552TEM>
2022-01-20 22:03:30 +0300

    Proje dosyası ekle.

---
+++ AspNetWebAPIOAuth/OAuth/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace AspNetWebAPIOAuth.OAuth.Providers
+{
+    
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        
+        KullaniciEntities1 db = new KullaniciEntities1();
+        public string username;
+        public string password;
+        // OAuthAuthorizationServerProvider sınıfının client erişimine izin verebilmek için ilgili ValidateClientAuthentication metotunu override ediyoruz.
+        
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+    
+        [HttpPost]
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+          
+            // CORS ayarlarını set ediyoruz.
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            kullanicilar yeni = new kullanicilar();
+            try
+            {
+                yeni = db.kullanicis.Where(w => w.username == context.UserName && w.pass == context.Password).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                yeni = null;
+            }
+            
+
+            List<kullanicilar> asamlist = new List<kullanicilar>();
+            asamlist = db.kullanicis.ToList();
+            foreach (var item in asamlist)
+            {
+
+                username = item.username;
+                password = item.pass;
+
+            }
+            db.SaveChanges();
+
+            // Kullanıcının access_token alabilmesi için gerekli validation işlemlerini yapıyoruz.
+            if (context.UserName == yeni.username && context.Password == yeni.pass)
+            {
+
+              
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Kullanıcı adı veya şifre yanlış.");
+            }
+           
+           
+        }
+    }
+}
---
+++ AspNetWebAPIOAuth/OAuth/Startup.cs
@@ -0,0 +1,46 @@
+﻿using AspNetWebAPIOAuth.OAuth.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Web.Http.Cors;
+
+[assembly: OwinStartup(typeof(AspNetWebAPIOAuth.OAuth.Startup))]
+namespace AspNetWebAPIOAuth.OAuth
+
+    
+{
+    [EnableCors(origins: "http://localhost:35013", headers: "*", methods: "*")]
+    // Servis çalışmaya başlarken Owin pipeline'ını ayağa kaldırabilmek için Startup'u hazırlıyoruz.
+    public class Startup
+    {
+       
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            ConfigureOAuth(appBuilder);
+
+            WebApiConfig.Register(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+
+        private void ConfigureOAuth(IAppBuilder appBuilder)
+        {
+            OAuthAuthorizationServerOptions oAuthAuthorizationServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new Microsoft.Owin.PathString("/token"), // token alacağımız path'i belirtiyoruz
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(1),
+                AllowInsecureHttp = true,
+                Provider = new SimpleAuthorizationServerProvider(),
+                RefreshTokenProvider= new SimpleRefreshTokenProvider()
+            };
+
+            // AppBuilder'a token üretimini gerçekleştirebilmek için ilgili authorization ayarlarımızı veriyoruz.
+            appBuilder.UseOAuthAuthorizationServer(oAuthAuthorizationServerOptions);
+            appBuilder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
